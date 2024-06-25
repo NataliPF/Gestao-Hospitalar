5b97c281f36a9ae893cc241ceb7fe944b576963c
Natali <nana_projetti@hotmail.com>
2024-06-25 19:59:50 -0300

    MVC Status e Paciente.

---
+++ listarStatus.php
@@ -0,0 +1,18 @@
+<?php
+
+include_once("services/conexao.php");
+include_once("controller/controllerStatus.php");
+include_once("model/modelStatus.php");
+
+$controllerStatus = new controllerStatus();
+$lista = $controllerStatus->listarStatus();
+
+if($lista) {
+    $msg = array("status" => $lista);
+    echo json_encode($msg);
+} else {
+    $msg = array("status" => []);
+    echo json_encode($msg);
+}
+
+?>
